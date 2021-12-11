65ecd00ee7d7bc5153b9b9cf2db518614648bc1e
faldymartinus <faldymartinus@gmail.com>
2021-12-11 07:29:43 +0700

    display bookmark

--- public/dashboard.js
+++ public/dashboard.js
@@ -17,6 +17,7 @@ firebase.auth().onAuthStateChanged(function(user) {
         })
     }
     displayData();
+    displayBookmarks();
   } else {
     // No user is signed in.
   }
@@ -25,7 +26,7 @@ firebase.auth().onAuthStateChanged(function(user) {
 function displayData(){
   firebase.database().ref('projects/').once('value').then(function(snapshot){
     var postDiv = document.getElementById('post');
-    post.innerHTML="";
+    
     var data = snapshot.val();
     console.log(data);
     
@@ -34,7 +35,6 @@ function displayData(){
     for(let[key,value] of Object.entries(data)){
       
       postDiv.innerHTML = 
-      // "<div class='card'>"+"<h2>"+value.caption+"</h2>"+"<br></br>"+"</div>"+postDiv.innerHTML;
           "<div class='u-container-style u-list-item u-repeater-item'>"
           +"<div class='u-container-layout u-similar-container u-container-layout-2'>"
           +  "<div class='u-absolute-hcenter u-container-style u-expanded u-group u-radius-10 u-shape-round u-white u-group-1'>"
@@ -52,6 +52,38 @@ function displayData(){
           +  "</div>"
           +  "</div>"
           +"</div>"+postDiv.innerHTML;
+    }
+  })
+}
+
+function displayBookmarks(){
+  firebase.database().ref('bookmarks/').once('value').then(function(snapshot){
+    var bookmarksDiv = document.getElementById('bookmarks');
+   
+    var data = snapshot.val();
+    console.log(data);
+    
+
+    //menampilkan semua Bookmarks
+    for(let[key,value] of Object.entries(data)){
+      firebase.database().ref('company/'+value.userId).once('value').then(function(snapshot){
+        var user = firebase.auth().currentUser;
+        const uid = user.uid;
+        var data = snapshot.val();
+
+        console.log(data);
+        bookmarksDiv.innerHTML = 
+            "<div class='u-container-style u-list-item u-repeater-item u-shape-rectangle'>"
+            +"  <div class='u-container-layout u-similar-container u-container-layout-10'>"
+            +"    <div class='u-custom-color-1 u-expanded-width u-radius-5 u-shape u-shape-round u-shape-2'></div>"
+            +"   <p class='u-small-text u-text u-text-body-alt-color u-text-default u-text-variant u-text-17'>3 Hours Ago</p>"
+            +"    <h1 class='u-text u-text-body-alt-color u-text-default u-title u-text-18'><b>"+data.company_name+"</b></h1>"
+            +"    <p class='u-large-text u-text u-text-variant u-text-white u-text-19'>Baby Supliers Online Shop</p>"
+            +"    <img class='u-image u-image-circle u-image-5' src='images/Foto_profil.jpg'  data-image-width='1280' data-image-height='852'></img>"
+            +"  </div>"
+            +"</div>"
+            +bookmarksDiv.innerHTML;
+      })
     }
   })
 }
